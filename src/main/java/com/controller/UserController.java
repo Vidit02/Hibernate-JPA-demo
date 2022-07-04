7674c3a4032748f47c75ee519cecc812e08958e8
“Vidit <vidit.21102@gmail.com>
2022-07-05 01:47:15 +0530

    Delete Update CRUD done

--- src/main/java/com/controller/UserController.java
+++ src/main/java/com/controller/UserController.java
@@ -1,12 +1,16 @@
 package com.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.apache.catalina.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.entity.UserEntity;
@@ -29,5 +33,22 @@ public class UserController {
 	public List<UserEntity> getAllUsers(){
 		List<UserEntity> users = userRepo.findAll();
 		return users;
+	}
+	
+	@DeleteMapping
+	public boolean removeUser(@RequestParam("userid") int userid) {
+		Optional<UserEntity> user = userRepo.findById(userid);
+		if(user.isPresent()) {
+			userRepo.deleteById(userid);
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	@PutMapping
+	public UserEntity updateUser(UserEntity user) {
+		userRepo.save(user);
+		return user;
 	}
 }
